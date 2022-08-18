529c1c6a664dce1b5108988049dae0d301dcd69d
Jorge Luis Shirai <jorgeshirai@gmail.com>
2022-08-18 12:25:18 -0300

    Add basic DarkModeToggler.vue.
    Set tailwind.config.js darkMode = 'class'.
    Add @vueuse/core dependency for the dark mode composables.
    Update index.html and App to use DarkModeToggler and basic dark:
    classes.

---
+++ src/components/DarkModeToggler.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    type="button"
+    @click="() => toggleDark()"
+    :title="isDark ? 'Switch to light mode' : 'Switch to dark mode'"
+    class="w-7 y-7 text-lg bg-gray-300 rounded-full dark:bg-gray-700"
+  >
+    {{ isDark ? '🌞' : '🌛' }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
