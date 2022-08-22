3922b042731d4f3b301435bf5edf1947ee8f3969
Jorge Luis Shirai <jorgeshirai@gmail.com>
2022-08-22 11:20:30 -0300

    Add countries' store unit test.

---
+++ src/stores/__tests__/countries.spec.ts
@@ -0,0 +1,52 @@
+import { beforeEach, describe, it, expect } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+import { useStore } from '../countries'
+import data from '@/assets/data.json'
+
+describe('Store: countries', () => {
+  let store
+  function getHighlighted(countries: any[]): any[] {
+    return countries.filter((c) => c.HIGHLIGHT === true)
+  }
+
+  beforeEach(() => {
+    // create a fresh pinia and make it active so it's automatically picked
+    // up by any useStore call without having to pass it to it: useStore(pinia)
+    setActivePinia(createPinia())
+    store = useStore()
+  })
+
+  it('initial state: population is "", countries is loaded with data', () => {
+    expect(store.population).toBe('')
+    expect(store.countries).toStrictEqual(data)
+  })
+
+  describe('Getter: filteredCountries', () => {
+    it("population: '', no country highlighted", () => {
+      expect(store.population).toBe('')
+      expect(getHighlighted(store.filteredCountries).length).toBe(0)
+    })
+
+    it("population: '0', 248 countries highlighted (there are 2 with 0)", () => {
+      store.population = '0'
+      // "Bouvet Island" and "Heard Island and McDonald Islands" have population
+      // === 0 in data.json
+      expect(getHighlighted(store.filteredCountries).length).toBe(248)
+    })
+
+    it("population: '1402112000', 0 countries highlighted (max population of data.json)", () => {
+      store.population = '1402112000'
+      expect(getHighlighted(store.filteredCountries).length).toBe(0)
+    })
+
+    it("population: '1000000', 160 countries highlighted (same as e2e)", () => {
+      store.population = '1000000'
+      expect(getHighlighted(store.filteredCountries).length).toBe(160)
+    })
+
+    it("population: '10000000', 91 countries highlighted (same as e2e)", () => {
+      store.population = '10000000'
+      expect(getHighlighted(store.filteredCountries).length).toBe(91)
+    })
+  })
+})
